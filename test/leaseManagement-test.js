f8bc0fda1619291442fc1728987b589988451681
Ömer Sayilir <omersayilir75@gmail.com>
2022-04-03 14:37:27 +0200

    added global user var to leasemanagement tests

--- test/leaseManagement-test.js
+++ test/leaseManagement-test.js
@@ -3,6 +3,8 @@ var chai = require('chai');
 var sinon = require('sinon');
 chai.use(require('sinon-chai'));
 
+
+
 //mocking
 const { mockRequest, mockResponse } = require('mock-req-res');
 
@@ -12,22 +14,62 @@ const leaseManagement = require('../src/user/leaseManagement.js');
 const util = require('./testUtil.js');
 const userManagement = require('../src/user/userManagement.js');
 
+var testuserID;
+
 describe('Lease management', function () {
     // not 100% sure that this works
     util.truncateDatabase();
 
-    const userID = 0; // assumed database to start counting from 0
-    await userManagement.registerUser(
-        mockRequest({ body: { email: 'hi@bye.com', password: 'password', name: 'Dirty Harry', } }),
-        {status: sinon.spy, send: sinon.spy},
-        util.foid
-    );
+    // const userID = 0; // assumed database to start counting from 0
+    // await userManagement.registerUser(
+    //     mockRequest({ body: { email: 'hi@bye.com', password: 'password', name: 'Dirty Harry', } }),
+    //     {status: sinon.spy, send: sinon.spy},
+    //     util.foid
+    // );
+
+            // register a user beforehand
+            it('should register a user for our tests', async function () {
+                const req = mockRequest({
+                    body: {
+                        email: 'hi@bye.com',
+                        password: 'password',
+                        name: 'Dirty Harry',
+                    },
+                });
+      
+                const res = {};
+                res.status = sinon.spy();
+                res.send = sinon.spy();
+      
+                const result = await userManagement.registerUser(req, res, () => {});
+      
+                chai.expect(res.status).to.have.been.calledWith(200); // status OK
+            });
+    
+            // log the user in
+            it('sign in as out registered user', async function () {
+      
+                var req = mockRequest({
+                    body: {
+                        email: 'hi@bye.com',
+                        password: 'password',
+                    },
+                });
+        
+                const result = await userManagement.requestLogin(req.body, () => {});
+    
+                testuserID = result.user.userID;
+    
+                chai.assert.equal(result.status, 200);//check request successful
+                chai.assert.equal(result.error, false); // check no error
+                chai.assert.notEqual(result.user, undefined);//check user correct
+            });
 
     describe("#registerLease", function () {
         it("Should do some stuff", async function () {
             const req = mockRequest({
                 body: {
-                    userID: userID,
+                    userID: testuserID,
                     capacity: 2,
                     price: 8,
                     startDate: 'today',
